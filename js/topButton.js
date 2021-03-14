970e0155cfed109dd5376534112d5db7b43d07bd
AlonsoGD <dani.alonso.garcia@gmail.com>
2021-03-14 00:49:35 -0500

    Multiple small changes

---
+++ js/topButton.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+//Get the button
+var goTopButton = document.getElementById("goTopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goTopButton.style.display = "block";
+  } else {
+    goTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+goTopButton.addEventListener("click", function(){
+  topFunction()
+});
+
+});
